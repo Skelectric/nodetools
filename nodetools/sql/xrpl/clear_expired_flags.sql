a47c34ff3e25e814e639a3719b1574b45ee71392
Skelectric <58760447+Skelectric@users.noreply.github.com>
2025-01-22 16:12:46 -0500

    Removed extraneous

--- nodetools/sql/xrpl/clear_expired_flags.sql
+++
@@ -1,8 +0,0 @@
-UPDATE authorized_addresses
-SET 
-    is_authorized = TRUE,
-    flag_type = NULL,
-    flag_expires_at = NULL
-WHERE 
-    flag_type IS NOT NULL 
-    AND flag_expires_at < CURRENT_TIMESTAMP;
--- nodetools/sql/xrpl/is_address_authorized.sql
+++
@@ -1,6 +0,0 @@
-SELECT EXISTS (
-    SELECT 1 
-    FROM authorized_addresses 
-    WHERE address = $1 
-    AND is_authorized = TRUE
-) as is_authorized;
